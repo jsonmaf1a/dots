f41ecd323a1f78fc6471dc837ec6d49003b48fc9
Kirill <kkpetrov.dev@gmail.com>
2024-11-24 20:43:35 +0200

    upd

--- .config/eww/widgets/workspaces/scripts/active-workspace.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-socat -u UNIX-CONNECT:$XDG_RUNTIME_DIR/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock - | while read -r line; do
-	hyprctl -j activeworkspace | jq --raw-output '.name'
-done
